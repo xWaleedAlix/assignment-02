97836530a4e873d4ed290008f8297cf3ec6dc306
Waleed-Ali <waleedali534363279@gmail.com>
2024-08-25 20:54:39 -0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 const page = () => {
   return(
     <div>
-      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="\man.png" alt="" width="384" height="512" ></img>
-  <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+     <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="assignment-02\man.png" alt="" width="384" height="512" />
+  <div className="pt-6 md:p-8 text-center space-y-4">
     <blockquote>
       <p className="text-lg font-medium">
         “Tailwind CSS is the only framework that I've seen scale
