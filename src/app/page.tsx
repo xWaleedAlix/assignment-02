1eba73157dbf8d8ece6a4d23fcd6f33aabaa6db4
Waleed-Ali <waleedali534363279@gmail.com>
2024-08-25 21:06:08 -0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const page = () => {
   return(
     <div>
      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="man.png" alt="" width="384" height="512" />
+  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="man.png" alt="" width="384" height="512"/>
   <div className="pt-6 md:p-8 text-center space-y-4">
     <blockquote>
       <p className="text-lg font-medium">
