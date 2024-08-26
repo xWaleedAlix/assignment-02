bdd5650c030e8fa84a2bf220dffb8d5f8e66e638
Waleed-Ali <waleedali534363279@gmail.com>
2024-08-25 21:39:16 -0700

    x

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,32 @@
 import React from 'react';
+import Image from 'next/image'; 
 
-const page = () => {
-  return(
+const Page = () => { 
+  return (
     <div>
-     <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-   <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/man.png" alt="A person" width="100"height="100"/>
-  <div className="pt-6 md:p-8 text-center space-y-4">
-    <blockquote>
-      <p className="text-lg font-medium">
-        Tailwind CSS is the only framework that I haveve seen scale
-        on large teams. Its easy to customize. adapts to any design
-        and the build size is tiny.
-      </p>
-    </blockquote>
-    <figcaption className="font-medium">
-      <div className="text-sky-500 dark:text-sky-400">
-        Sarah Dayan
-      </div>
-      <div className="text-slate-700 dark:text-slate-500">
-        Staff Engineer Algolia
-      </div>
-    </figcaption>
-  </div>
-</figure>
+      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+        <Image 
+          className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/man.png" alt="A person" width="100"height="100"/>
+        <div className="pt-6 md:p-8 text-center space-y-4">
+          <blockquote>
+            <p className="text-lg font-medium">
+              Tailwind CSS is the only framework that I've seen scale
+              on large teams. It’s easy to customize, adapts to any design,
+              and the build size is tiny.
+            </p>
+          </blockquote>
+          <figcaption className="font-medium">
+            <div className="text-sky-500 dark:text-sky-400">
+              Sarah Dayan
+            </div>
+            <div className="text-slate-700 dark:text-slate-500">
+              Staff Engineer, Algolia
+            </div>
+          </figcaption>
+        </div>
+      </figure>
     </div>
   )
 }
 
-export default page
+export default Page;
